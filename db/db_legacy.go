5dfd5abe615de5819bf34cd79fc2444d2b912051
Tao Yang <yangtao3601@126.com>
2018-05-03 17:52:36 +0800

    fix

--- db/db_legacy.go
+++ db/db_legacy.go
@@ -37,20 +37,7 @@ func Get_All_Zeropoints() (string, error) {
     fmt.Println("in Get_All_Zeropoints")
     
     sm := new(sql.ZeroPointSqlMapper)
-    err := sm.RegisterSqlMapper()
-    if err!=nil {
-        return "", err
-    }
-
-    res, err := sql.Db_query(sm.Id, DBName)
-    if err!=nil {
-        fmt.Printf("query fails")
-        return "", err
-    }
-
-    fmt.Println(res)
-    fmt.Println("get_all_metadatas OK")
-    return res, nil
+    return RegisterAndQueryAll(sm)
 }
 
 func Get_All_Bookprograms() (string, error){
