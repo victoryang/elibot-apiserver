2d0434753362aa6b34aff5373b1c877dbff73399
Tao Yang <yangtao3601@126.com>
2018-05-04 15:55:47 +0800

    fix

--- api/v1/api.go
+++ api/v1/api.go
@@ -222,7 +222,7 @@ func RegisterV1(r *mux.Router) http.Handler {
 	r.HandleFunc("/", hello).Methods("GET")
 	r.HandleFunc("/v1/test", test).Methods("GET")
 	r.HandleFunc("/v1/arc", getAllArc).Methods("GET")
-	r.HandleFunc("/v1/arc", getArcParams).Methods("GET").Queries("file_no", "group")
+	r.HandleFunc("/v1/arcParams", getArcParams).Methods("GET").Queries("file_no", "group")
 	//r.HandleFunc("/v1/backup", getAllBackup).Methods("GET")
 	r.HandleFunc("/v1/bookprograms", getAllBookprograms).Methods("GET")
 	//r.HandleFunc("/v1/enum", getAllEnum).Methods("GET")
