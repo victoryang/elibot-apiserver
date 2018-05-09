0dc7ca1ac7c694934b167c6d705b565341e065fd
Tao Yang <yangtao3601@126.com>
2018-05-09 09:39:55 +0800

    fix

--- test/query_test.go
+++ test/query_test.go
@@ -6,6 +6,16 @@ import (
 	"fmt"
 )
 
+func Test_Get_root(t *testing.T) {
+	resp, err := http.Get("http://127.0.0.1:9000/")
+	if err!=nil {
+		t.Error("test failed")
+	} else {
+		t.Log("test pass")
+		fmt.Println(resp)
+	}
+}
+
 func Test_Get_all_arc(t *testing.T) {
 	resp, err := http.Get("http://127.0.0.1:9000/v1/arc")
 	if err!=nil {
@@ -16,7 +26,7 @@ func Test_Get_all_arc(t *testing.T) {
 	}
 }
 
-func Test_Get_all_arc(t *testing.T) {
+func Test_Get_all_arcparams(t *testing.T) {
 	resp, err := http.Get("http://127.0.0.1:9000/v1/arcparams")
 	if err!=nil {
 		t.Error("test failed")
@@ -26,7 +36,7 @@ func Test_Get_all_arc(t *testing.T) {
 	}
 }
 
-func Test_Get_all_arc(t *testing.T) {
+func Test_Get_all_bookprograms(t *testing.T) {
 	resp, err := http.Get("http://127.0.0.1:9000/v1/bookprograms")
 	if err!=nil {
 		t.Error("test failed")
@@ -36,7 +46,7 @@ func Test_Get_all_arc(t *testing.T) {
 	}
 }
 
-func Test_Get_all_arc(t *testing.T) {
+func Test_Get_all_extaxis(t *testing.T) {
 	resp, err := http.Get("http://127.0.0.1:9000/v1/extaxis")
 	if err!=nil {
 		t.Error("test failed")
@@ -46,7 +56,7 @@ func Test_Get_all_arc(t *testing.T) {
 	}
 }
 
-func Test_Get_all_arc(t *testing.T) {
+func Test_Get_all_interference(t *testing.T) {
 	resp, err := http.Get("http://127.0.0.1:9000/v1/interference")
 	if err!=nil {
 		t.Error("test failed")
@@ -56,7 +66,7 @@ func Test_Get_all_arc(t *testing.T) {
 	}
 }
 
-func Test_Get_all_arc(t *testing.T) {
+func Test_Get_all_metadata(t *testing.T) {
 	resp, err := http.Get("http://127.0.0.1:9000/v1/metadata")
 	if err!=nil {
 		t.Error("test failed")
@@ -66,8 +76,8 @@ func Test_Get_all_arc(t *testing.T) {
 	}
 }
 
-func Test_Get_all_arc(t *testing.T) {
-	resp, err := http.Get("http://127.0.0.1:9000/v1/parameter")
+func Test_Get_all_parameterbyid(t *testing.T) {
+	resp, err := http.Get("http://127.0.0.1:9000/v1/parameterbyid")
 	if err!=nil {
 		t.Error("test failed")
 	} else {
@@ -76,7 +86,17 @@ func Test_Get_all_arc(t *testing.T) {
 	}
 }
 
-func Test_Get_all_arc(t *testing.T) {
+func Test_Get_all_parameterbygroup(t *testing.T) {
+	resp, err := http.Get("http://127.0.0.1:9000/v1/parameterbygroup")
+	if err!=nil {
+		t.Error("test failed")
+	} else {
+		t.Log("test pass")
+		fmt.Println(resp)
+	}
+}
+
+func Test_Get_all_ref(t *testing.T) {
 	resp, err := http.Get("http://127.0.0.1:9000/v1/ref")
 	if err!=nil {
 		t.Error("test failed")
@@ -86,7 +106,7 @@ func Test_Get_all_arc(t *testing.T) {
 	}
 }
 
-func Test_Get_all_arc(t *testing.T) {
+func Test_Get_all_toolframe(t *testing.T) {
 	resp, err := http.Get("http://127.0.0.1:9000/v1/toolframe")
 	if err!=nil {
 		t.Error("test failed")
@@ -96,7 +116,7 @@ func Test_Get_all_arc(t *testing.T) {
 	}
 }
 
-func Test_Get_all_arc(t *testing.T) {
+func Test_Get_all_userframe(t *testing.T) {
 	resp, err := http.Get("http://127.0.0.1:9000/v1/userframe")
 	if err!=nil {
 		t.Error("test failed")
@@ -106,7 +126,7 @@ func Test_Get_all_arc(t *testing.T) {
 	}
 }
 
-func Test_Get_all_arc(t *testing.T) {
+func Test_Get_all_zeropoints(t *testing.T) {
 	resp, err := http.Get("http://127.0.0.1:9000/v1/zeropoints")
 	if err!=nil {
 		t.Error("test failed")
