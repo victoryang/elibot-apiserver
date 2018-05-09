22d1500dbbc33911ae59b7c40b275b68f33ff4d7
Tao Yang <yangtao3601@126.com>
2018-05-09 09:32:19 +0800

    fix

--- test/query_test.go
+++ test/query_test.go
@@ -7,10 +7,11 @@ import (
 )
 
 func Test_Get_all_arc(t *testing.T) {
-	resp, err := http.Get("curl 127.0.0.1:9000/v1/arc")
+	resp, err := http.Get("http://127.0.0.1:9000/v1/arc")
 	if err!=nil {
 		t.Error("test failed")
+	} else {
+		t.Log("test pass")
+		fmt.Println(resp)
 	}
-	t.Log("test pass")
-	fmt.Println(resp)
 }
