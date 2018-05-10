71c9b5d5e4f945331d25552f0af4c2757159b6d9
Tao Yang <yangtao3601@126.com>
2018-05-10 15:54:52 +0800

    fix

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func handleSignals(server *api.Server) {
 
 func main() {
 	c := config.NewConfiguration()
-	c.GlobalConfig = config.LoadFile(c.ConfigFile)
+	c.GlobalConfig,_ = config.LoadFile(c.ConfigFile)
 
 	s := api.NewApiServer(c.GlobalConfig)
 	s.Run();
