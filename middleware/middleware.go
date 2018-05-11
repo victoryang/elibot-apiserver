9a4a17ae5c1df503967b2365a44e8fc00aa0e32b
Tao Yang <yangtao3601@126.com>
2018-05-11 16:03:56 +0800

    fix

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,10 +1,12 @@
 package middleware
 
 import (
+	Log "elibot-apiserver/log"
+	"elibot-apiserver/middleware/accesslog"
 	"github.com/urfave/negroni"
 )
 
-func AddAccesslog(n *Negroni) {
+func AddAccesslog(n *negroni.Negroni) {
 	alogger, err := accesslog.NewAccessLog(accesslogfile)
 	if err!=nil {
 		Log.Error("Error in opening access log file： ", err)
