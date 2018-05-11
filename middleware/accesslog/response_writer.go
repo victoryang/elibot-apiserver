3a2f4edc80343babd4f2164963f59e4bf5866d90
Tao Yang <yangtao3601@126.com>
2018-05-11 16:43:13 +0800

    fix

--- middleware/accesslog/response_writer.go
+++ middleware/accesslog/response_writer.go
@@ -10,96 +10,34 @@ import (
 // ResponseWriter is a wrapper around http.ResponseWriter that provides extra information about
 // the response. It is recommended that middleware handlers use this construct to wrap a responsewriter
 // if the functionality calls for it.
-type ResponseWriter interface {
+type ResponseWriter struct {
+	status int 
 	http.ResponseWriter
-	http.Flusher
-	// Status returns the status code of the response or 0 if the response has
-	// not been written
-	Status() int
-	// Written returns whether or not the ResponseWriter has been written.
-	Written() bool
-	// Size returns the size of the response body.
-	Size() int
-	// Before allows for a function to be called before the ResponseWriter has been written to. This is
-	// useful for setting headers or any other operations that must happen before a response has been written.
-	Before(func(ResponseWriter))
 }
 
-type beforeFunc func(ResponseWriter)
-
-// NewResponseWriter creates a ResponseWriter that wraps an http.ResponseWriter
-func NewResponseWriter(rw http.ResponseWriter) ResponseWriter {
-	nrw := &responseWriter{
-		ResponseWriter: rw,
-	}
-
-	if _, ok := rw.(http.CloseNotifier); ok {
-		return &responseWriterCloseNotifer{nrw}
-	}
-
-	return nrw
+func NewResponseWriter(res http.ResponseWriter) *MyResponseWriter {
+	// Default the status code to 200
+	return &ResponseWriter{200, res}
 }
 
-type responseWriter struct {
-	http.ResponseWriter
-	status      int
-	size        int
-	beforeFuncs []beforeFunc
+// Give a way to get the status
+func (w ResponseWriter) Status() int {
+	return w.status
 }
 
-func (rw *responseWriter) WriteHeader(s int) {
-	rw.status = s
-	rw.callBefore()
-	rw.ResponseWriter.WriteHeader(s)
+// Satisfy the http.ResponseWriter interface
+func (w ResponseWriter) Header() http.Header {
+	return w.ResponseWriter.Header()
 }
 
-func (rw *responseWriter) Write(b []byte) (int, error) {
-	if !rw.Written() {
-		// The status will be StatusOK if WriteHeader has not been called yet
-		rw.WriteHeader(http.StatusOK)
-	}
-	size, err := rw.ResponseWriter.Write(b)
-	rw.size += size
-	return size, err
+func (w ResponseWriter) Write(data []byte) (int, error) {
+	return w.ResponseWriter.Write(data)
 }
 
-func (rw *responseWriter) Status() int {
-	return rw.status
-}
+func (w ResponseWriter) WriteHeader(statusCode int) {
+	// Store the status code
+	w.status = statusCode
 
-func (rw *responseWriter) Size() int {
-	return rw.size
-}
-
-func (rw *responseWriter) Written() bool {
-	return rw.status != 0
-}
-
-func (rw *responseWriter) Before(before func(ResponseWriter)) {
-	rw.beforeFuncs = append(rw.beforeFuncs, before)
-}
-
-func (rw *responseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
-	hijacker, ok := rw.ResponseWriter.(http.Hijacker)
-	if !ok {
-		return nil, nil, fmt.Errorf("the ResponseWriter doesn't support the Hijacker interface")
-	}
-	return hijacker.Hijack()
-}
-
-func (rw *responseWriter) callBefore() {
-	for i := len(rw.beforeFuncs) - 1; i >= 0; i-- {
-		rw.beforeFuncs[i](rw)
-	}
-}
-
-func (rw *responseWriter) Flush() {
-	flusher, ok := rw.ResponseWriter.(http.Flusher)
-	if ok {
-		if !rw.Written() {
-			// The status will be StatusOK if WriteHeader has not been called yet
-			rw.WriteHeader(http.StatusOK)
-		}
-		flusher.Flush()
-	}
+	// Write the status code onward.
+	w.ResponseWriter.WriteHeader(statusCode)
 }
