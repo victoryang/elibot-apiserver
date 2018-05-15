e6189d87d3a44c715d023ebc865bb2b3eb64ffdc
Tao Yang <yangtao3601@126.com>
2018-05-15 11:28:28 +0800

    fix

--- middleware/accesslog/response_writer.go
+++ middleware/accesslog/response_writer.go
@@ -8,30 +8,30 @@ import (
 // the response. It is recommended that middleware handlers use this construct to wrap a responsewriter
 // if the functionality calls for it.
 type ResponseWriter struct {
-	status int 
 	http.ResponseWriter
+	status int
 }
 
 func NewResponseWriter(res http.ResponseWriter) *ResponseWriter {
 	// Default the status code to 200
-	return &ResponseWriter{200, res}
+	return &ResponseWriter{res, 200}
 }
 
 // Give a way to get the status
-func (w ResponseWriter) Status() int {
+func (w *ResponseWriter) Status() int {
 	return w.status
 }
 
 // Satisfy the http.ResponseWriter interface
-func (w ResponseWriter) Header() http.Header {
+func (w *ResponseWriter) Header() http.Header {
 	return w.ResponseWriter.Header()
 }
 
-func (w ResponseWriter) Write(data []byte) (int, error) {
+func (w *ResponseWriter) Write(data []byte) (int, error) {
 	return w.ResponseWriter.Write(data)
 }
 
-func (w ResponseWriter) WriteHeader(statusCode int) {
+func (w *ResponseWriter) WriteHeader(statusCode int) {
 	// Store the status code
 	w.status = statusCode
 
