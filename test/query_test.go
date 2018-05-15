1d7f652a5263d303e0712c19b74577323161ad29
Tao Yang <yangtao3601@126.com>
2018-05-15 13:28:00 +0800

    fix

--- test/query_test.go
+++ test/query_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"net/http"
 	"fmt"
+	"time"
 )
 
 const ipAddr = "http://127.0.0.1"
@@ -15,56 +16,72 @@ func GetIP(p string) string {
 }
 
 func Test_Get_root(t *testing.T) {
+	start := time.Now()
 	resp, err := http.Get(ipAddr + port + "/")
 	if err!=nil {
 		t.Error("test failed")
 	} else {
 		t.Log("test pass")
+		d := time.Since(start)
 		fmt.Println(resp)
+		fmt.Println("time: ", d)
 	}
 }
 
 func Test_Get_all_arc(t *testing.T) {
+	start := time.Now()
 	resp, err := http.Get(GetIP("/arc"))
 	if err!=nil {
 		t.Error("test failed")
 	} else {
 		t.Log("test pass")
+		d := time.Since(start)
 		fmt.Println(resp)
+		fmt.Println("time: ", d)
 	}
 }
 
 func Test_Get_all_arcparams(t *testing.T) {
+	start := time.Now()
 	resp, err := http.Get(GetIP("/arcparams")+"?group=arc.welder")
 	if err!=nil {
 		t.Error("test failed")
 	} else {
 		t.Log("test pass")
+		d := time.Since(start)
 		fmt.Println(resp)
+		fmt.Println("time: ", d)
 	}
 }
 
 func Test_Get_all_bookprograms(t *testing.T) {
+	start := time.Now()
 	resp, err := http.Get(GetIP("/bookprograms"))
 	if err!=nil {
 		t.Error("test failed")
 	} else {
 		t.Log("test pass")
+		d := time.Since(start)
 		fmt.Println(resp)
+		fmt.Println("time: ", d)
 	}
 }
 
 func Test_Get_all_enum(t *testing.T) {
+	start := time.Now()
 	resp, err := http.Get(GetIP("/enum"))
 	if err!=nil {
 		t.Error("test failed")
 	} else {
 		t.Log("test pass")
+		d := time.Since(start)
 		fmt.Println(resp)
+		fmt.Println("time: ", d)
 	}
 }
 
 func Test_Get_all_extaxis(t *testing.T) {
+	start := time.Now()
 	resp, err := http.Get(GetIP("/extaxis"))
 	if err!=nil {
 		t.Error("test failed")
@@ -75,92 +92,119 @@ func Test_Get_all_extaxis(t *testing.T) {
 }
 
 func Test_Get_all_interference(t *testing.T) {
+	start := time.Now()
 	resp, err := http.Get(GetIP("/interference"))
 	if err!=nil {
 		t.Error("test failed")
 	} else {
 		t.Log("test pass")
+		d := time.Since(start)
 		fmt.Println(resp)
+		fmt.Println("time: ", d)
 	}
 }
 
 func Test_Get_all_metadata(t *testing.T) {
+	start := time.Now()
 	resp, err := http.Get(GetIP("/metadata")+"?lang=zh_cn")
 	if err!=nil {
 		t.Error("test failed")
 	} else {
 		t.Log("test pass")
+		d := time.Since(start)
 		fmt.Println(resp)
+		fmt.Println("time: ", d)
 	}
 }
 
 func Test_Get_all_params(t *testing.T) {
+	start := time.Now()
 	resp, err := http.Get(GetIP("/params"))
 	if err!=nil {
 		t.Error("test failed")
 	} else {
 		t.Log("test pass")
+		d := time.Since(start)
 		fmt.Println(resp)
+		fmt.Println("time: ", d)
 	}
 }
 
 func Test_Get_all_parameterbyid(t *testing.T) {
+	start := time.Now()
 	resp, err := http.Get(GetIP("/parameterbyid")+"?md_id=param.speed.speed_min_joint")
 	if err!=nil {
 		t.Error("test failed")
 	} else {
 		t.Log("test pass")
+		d := time.Since(start)
 		fmt.Println(resp)
+		fmt.Println("time: ", d)
 	}
 }
 
 func Test_Get_all_parameterbygroup(t *testing.T) {
+	start := time.Now()
 	resp, err := http.Get(GetIP("/parameterbygroup")+"?group=param.speed")
 	if err!=nil {
 		t.Error("test failed")
 	} else {
 		t.Log("test pass")
+		d := time.Since(start)
 		fmt.Println(resp)
+		fmt.Println("time: ", d)
 	}
 }
 
 func Test_Get_all_ref(t *testing.T) {
+	start := time.Now()
 	resp, err := http.Get(GetIP("/ref"))
 	if err!=nil {
 		t.Error("test failed")
 	} else {
 		t.Log("test pass")
+		d := time.Since(start)
 		fmt.Println(resp)
+		fmt.Println("time: ", d)
 	}
 }
 
 func Test_Get_all_toolframe(t *testing.T) {
+	start := time.Now()
 	resp, err := http.Get(GetIP("/toolframe"))
 	if err!=nil {
 		t.Error("test failed")
 	} else {
 		t.Log("test pass")
+		d := time.Since(start)
 		fmt.Println(resp)
+		fmt.Println("time: ", d)
 	}
 }
 
 func Test_Get_all_userframe(t *testing.T) {
+	start := time.Now()
 	resp, err := http.Get(GetIP("/userframe"))
 	if err!=nil {
 		t.Error("test failed")
 	} else {
 		t.Log("test pass")
+		d := time.Since(start)
 		fmt.Println(resp)
+		fmt.Println("time: ", d)
 	}
 }
 
 func Test_Get_all_zeropoints(t *testing.T) {
+	start := time.Now()
 	resp, err := http.Get(GetIP("/zeropoints"))
 	if err!=nil {
 		t.Error("test failed")
 	} else {
 		t.Log("test pass")
+		d := time.Since(start)
 		fmt.Println(resp)
+		fmt.Println("time: ", d)
 	}
 }
 
