3ddeb75e0521f12b41f2af9fe17b96c4c7c536b3
Tao Yang <yangtao3601@126.com>
2018-05-18 11:50:30 +0800

    fix

--- package/build_on_arm.sh
+++ package/build_on_arm.sh
@@ -13,9 +13,10 @@ sudo apt-get install gcc-arm-linux-gnueabi
 sudo apt-get install g++-arm-linux-gnueabi
 
 # Install libz.so.1 for dependency of libsqlitedb.so
-sudo apt-get install lib64z1
+sudo apt-get install lib64z1 # Here should replace the libz with competible one to libsqlitedb.so, which one should be compiled by ourselves
+
 ln -s /usr/arm-linux-gnueabi/lib/libz.so.1.2.8 /usr/lib/gcc-cross/arm-linux-gnueabihf/5/../../../../arm-linux-gnueabihf/lib/libz.so.1
 
 #env GOOS=linux GOARCH=arm64 go build -o elibot-server
 
-CC=arm-linux-gnueabihf-gcc GOOS=linux GOARCH=arm GOARM=6 CGO_ENABLED=1 go build -v -o /root/elibot-server #-ldflags="-extld=$CC"
+CC=arm-linux-gnueabihf-gcc GOOS=linux GOARCH=arm GOARM=6 CGO_ENABLED=1 go build -v -o /root/elibot-server
