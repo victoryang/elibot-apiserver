59de7448cd4d6370b92f5f84f5f5e78766d59143
Tao Yang <yangtao3601@126.com>
2018-05-30 11:26:55 +0800

    fix

---
+++ client/hello_client.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+
+#include "hello.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using serverpb::Req;
+using serverpb::Reply;
+using serverpb::Greeter;
+
+class GreeterClient {
+ public:
+  GreeterClient(std::shared_ptr<Channel> channel)
+      : stub_(Greeter::NewStub(channel)) {}
+
+  // Assembles the client's payload, sends it and presents the response back
+  // from the server.
+  std::string SayHello(::google::protobuf::int32& value) {
+    // Data we are sending to the server.
+    Req request;
+    request.set_name(value);
+
+    // Container for the data we expect from the server.
+    Reply reply;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The actual RPC.
+    Status status = stub_->SayHello(&context, request, &reply);
+
+    // Act upon its status.
+    if (status.ok()) {
+      return reply.message();
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return "RPC failed";
+    }
+  }
+
+ private:
+  std::unique_ptr<Greeter::Stub> stub_;
+};
+
+int main(int argc, char** argv) {
+  // Instantiate the client. It requires a channel, out of which the actual RPCs
+  // are created. This channel models a connection to an endpoint (in this case,
+  // localhost at port 50051). We indicate that the channel isn't authenticated
+  // (use of InsecureChannelCredentials()).
+  GreeterClient greeter(grpc::CreateChannel(
+      "localhost:2500", grpc::InsecureChannelCredentials()));
+
+  std::string reply = greeter.SayHello(1);
+  std::cout << "Greeter received: " << reply << std::endl;
+
+  return 0;
+}
