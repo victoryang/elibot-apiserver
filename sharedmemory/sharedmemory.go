98e047d7b6bb9691c1bc95d962c2953b4da1e158
Tao Yang <yangtao3601@126.com>
2018-06-08 13:42:59 +0800

    fix

--- sharedmemory/sharedmemory.go
+++ sharedmemory/sharedmemory.go
@@ -9,19 +9,43 @@ import(
 	"bytes"
 	"fmt"
 	"encoding/binary"
+	"time"
 	"elibot-apiserver/api"
 )
 
 var wss *api.WsServer = nil
+var old []byte
+const (
+	duration = 5
+	watchPeriod = time.Second * duration
+)
 
-func getPressReset() error {
+func getPressReset() []byte, error {
 	value := C.get_press_reset()
 	fmt.Println("Get press reset value: ", value)
 	b := make([]byte, 2)
 	buf := bytes.NewBuffer(b)
-	binary.Write(buf, binary.LittleEndian, value)
-	wss.Hub.PushMsg(buf.Bytes())
-	return nil
+	err := binary.Write(buf, binary.LittleEndian, value)
+	if err!=nil {
+		return []byte{}, err
+	}
+	return buf.Bytes(), nil
+}
+
+func watcher() {
+	watchTicker := time.NewTicker(watchPeriod)
+	for {
+		select {
+		case <-watchTicker.C:
+			data, err := getPressReset()
+			if err!=nil {
+				fmt.Println(err)
+			} else if data != old {
+				old = data
+				wss.Hub.PushMsg(data)
+			}
+		}
+	}
 }
 
 func handleMsg(msg []byte) {
@@ -31,5 +55,6 @@ func handleMsg(msg []byte) {
 func NewAndWatch(s *api.WsServer) {
 	wss = s
 	wss.Hub.NotificationRegister(handleMsg)
+	go watcher()
 	return
 }
