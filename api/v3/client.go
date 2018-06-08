1602a43c0f117a1672eeec85371746d08e4c9435
Tao Yang <yangtao3601@126.com>
2018-06-08 11:44:46 +0800

    fix

--- api/v3/client.go
+++ api/v3/client.go
@@ -66,7 +66,7 @@ func (c *Client) readPump() {
                         break
                 }
                 message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
-                c.hub.registerFunc(msg)
+                c.hub.registerFunc(message)
         }
 }
 
