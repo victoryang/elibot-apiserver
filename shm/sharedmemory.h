9a2ae2a1d2984fcd47edd3d19ed07553c89ba1f0
Tao Yang <yangtao3601@126.com>
2018-06-12 12:49:02 +0800

    fix

--- shm/sharedmemory.h
+++ shm/sharedmemory.h
@@ -1,18 +1,10 @@
 #ifndef SHAREDMEMORY_H
 #define SHAREDMEMORY_H
 
-#include "share/resource.h"
+int get_press_reset();
 
-int get_press_reset() {
-	return PRESS_RESET;
-}
+int32_t get_zero_encode(const int x);
 
-int32_t get_zero_encode(const int x) {
-	return Get_ZeroEncode(x);
-}
+void init_shm();
 
-void init_shm() {
-	resource_init("/rbctrl/mcserver");
-	init_nvram(NULL);
-}
 #endif //SHAREDMEMORY_H
