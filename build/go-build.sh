7caa68a9c1276b17237aa988b48ba74b05d4ee4b
Tao Yang <yangtao3601@126.com>
2018-06-12 16:52:20 +0800

    fix

--- build/go-build.sh
+++ build/go-build.sh
@@ -1,3 +1,26 @@
 #!/bin/sh
 
-go build -o elibot-server
+BASE=elibot-server
+API=api
+API_V1=${BASE}/${API}/v1
+API_V2=${BASE}/${API}/v2
+API_V3=${BASE}/${API}/v3
+CONFIG=${BASE}/config
+DB=${BASE}/db
+LOG=${BASE}/log
+MCSERVER=${BASE}/mcserver
+MIDDLEWARE=${BASE}/middleware
+SERVERPB=${BASE}/serverpb
+SQLITEDB=${BASE}/sqlitedb
+VENDOR=${BASE}/vendor
+
+sed -i '/\"elibot-apiserver\/shm\"/d' main.go
+sed -i '/shms.Shutdown()/d' main.go
+sed -i '/shms := shm.NewServer(wss)/d' main.go
+sed -i '/shms.StartToWatch()/d' main.go
+sed -i 's/handleSignals(s, mcs, gs, wss, shms)/handleSignals(s, mcs, gs, wss)/g' main.go
+sed -i 's/, shms \*shm.ShmServer//g' main.go
+
+go build -o elibot-server ${API_V1}/* ${API_V2}/* ${API_V3}/* ${API}/* ${CONFIG}/* \ 
+							${DB}/* ${LOG}/* ${MCSERVER}/* ${MIDDLEWARE}/* ${SERVERPB}/* ${SQLITEDB}/* \
+							${VENDOR}/*
