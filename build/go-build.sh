b14d54368daa1154b79d9ab833bad55e8fb4429a
Tao Yang <yangtao3601@126.com>
2018-06-12 17:12:54 +0800

    fix

--- build/go-build.sh
+++ build/go-build.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
 
-BASE=elibot-server
-API=api
-API_V1=${BASE}/${API}/v1
-API_V2=${BASE}/${API}/v2
-API_V3=${BASE}/${API}/v3
-CONFIG=${BASE}/config
-DB=${BASE}/db
-LOG=${BASE}/log
-MCSERVER=${BASE}/mcserver
-MIDDLEWARE=${BASE}/middleware
-SERVERPB=${BASE}/serverpb
-SQLITEDB=${BASE}/sqlitedb
-VENDOR=${BASE}/vendor
-
 sed -i '/\"elibot-apiserver\/shm\"/d' main.go
 sed -i '/shms.Shutdown()/d' main.go
 sed -i '/shms := shm.NewServer(wss)/d' main.go
@@ -21,4 +7,4 @@ sed -i '/shms.StartToWatch()/d' main.go
 sed -i 's/handleSignals(s, mcs, gs, wss, shms)/handleSignals(s, mcs, gs, wss)/g' main.go
 sed -i 's/, shms \*shm.ShmServer//g' main.go
 
-go build -o elibot-server ${API_V1}/*.go ${API_V2}/*.go ${API_V3}/*.go ${API}/*.go ${CONFIG}/*.go ${DB}/*.go ${LOG}/*.go ${MCSERVER}/*.go ${MIDDLEWARE}/*.go ${SERVERPB}/*.go ${SQLITEDB}/*.go ${VENDOR}/*.go
+go build -o elibot-server
