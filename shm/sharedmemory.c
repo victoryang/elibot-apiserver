d5c8844ccbfe7ffda5de59192a5548cefee48f4d
Tao Yang <yangtao3601@126.com>
2018-06-13 14:21:47 +0800

    fix

--- shm/sharedmemory.c
+++ shm/sharedmemory.c
@@ -1,11 +1,13 @@
-#include "share/resource.h"
-
 int get_press_reset() {
 	return PRESS_RESET;
 }
 
 int32_t get_zero_encode(const int x) {
 	return Get_ZeroEncode(x);
+}
+
+void GetAll() {
+
 }
 
 void init_shm() {
--- shm/sharedmemory.h
+++ shm/sharedmemory.h
@@ -1,10 +1,14 @@
 #ifndef SHAREDMEMORY_H
 #define SHAREDMEMORY_H
+#include "share/resource.h"
+#include "crc.h"
 
 int get_press_reset();
 
 int32_t get_zero_encode(const int x);
 
 void init_shm();
+
+void GetAll();
 
 #endif //SHAREDMEMORY_H
