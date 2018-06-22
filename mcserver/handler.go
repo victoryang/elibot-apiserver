971ff24984a76c101c9f71d87c254e7299bc2664
Tao Yang <yangtao3601@126.com>
2018-06-22 14:30:53 +0800

    fix

--- mcserver/handler.go
+++ mcserver/handler.go
@@ -21,17 +21,6 @@ type Handler struct {
 	conn 		net.Conn
 }
 
-func parse1(s string) string {
-    var res string
-    ss := strings.Split(s, "\n")
-    for _, v := range ss {
-        if v != CMDLINE {
-            res+=v
-        }
-    }
-    return res
-}
-
 func (h *Handler)HandleCommand(ctx context.Context, command string) (string, error) {
 	err := writeline(h.conn, command)
 	if err!=nil {
