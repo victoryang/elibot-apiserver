e25d0e69ed9c235d187e74e87aff696e372fae6a
Tao Yang <yangtao3601@126.com>
2018-07-10 16:59:44 +0800

    fix

--- api/apiv2/api-response.go
+++ api/apiv2/api-response.go
@@ -3,15 +3,30 @@ package apiv2
 import (
 	"net/http"
 	"fmt"
+	"encoding/json"
 )
 
+type Response struct {
+	Res 		string			`json:"res"`
+}
+
+func ToJson(res string) {
+	response := Response{
+		Res:	res,
+	}
+	r, _ := json.Marshal(response)
+	return r
+}
+
 // WriteSuccessResponse write success headers and response if any.
-func WriteSuccessResponse(w http.ResponseWriter, response string) {
+func WriteSuccessResponse(w http.ResponseWriter, res string) {
+	r := ToJson(res)
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, response)
+	w.Write(r)
 }
 
 func WriteInternalServerErrorResponse(w http.ResponseWriter, err error) {
+	r := ToJson(err.Error())
 	w.WriteHeader(http.StatusInternalServerError)
-	fmt.Fprintf(w, err.Error())
+	w.Write(r)
 }
