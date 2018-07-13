88fd6a2667de68420e6e7e6443df43fd57c0c7e1
Tao Yang <yangtao3601@126.com>
2018-07-13 14:51:55 +0800

    add apidoc

--- doc/apidoc/inputspecs/repository.go
+++ doc/apidoc/inputspecs/repository.go
@@ -1,5 +1,18 @@
 /**
-* @api {put} /v2/robot/repository/arcparam/:file_no/:md_id Set arc parameters
+* @apiDefine RequestBodyIndexAndValue
+*
+* @apiParam (params) {string} index    Body Content: index
+* @apiParam (params) {string} value    Body Content: value
+*
+* @apiParamExample {json} Request-Body (example):
+* 		{
+*			"index": "0",
+*			"value": "0"
+*		}
+*/
+
+/**
+* @api {put} /v2/robot/repository/arcparam/:file_no/:md_id setArcParam
 * @apiVersion 0.1.0
 * @apiName setArcParam
 * @apiGroup Repository
@@ -9,31 +22,110 @@
 * @apiExample Example usage:
 * curl -X PUT http://192.168.1.253:9000/v2/robot/repository/arcparam/:file_no/:md_id
 *
-* @apiHeader {string="application/json"} Content-Type=application/json;charset=utf-8 only support json body content
-*
-* @apiHeaderExample {json} Request-Header-Example:
-* 		{
-*			"Content-Type": "application/json;charset=utf-8"
-*		}
+* @apiUse DefaultHeaderAndExample
 *
 * @apiParam (params) {string} file_no  URL Param: file to set
 * @apiParam (params) {string} md_id    URL Param: metadata id
-* @apiParam (params) {string} index    Body Content: index
-* @apiParam (params) {string} value    Body Content: value
+* @apiUse RequestBodyIndexAndValue
 *
-* @apiParamExample {json} Request-Body (example):
-* 		{
-*			"index": "0",
-*			"value": "0"
-*		}
+* @apiUse DefaultResponsesAndExample
+*/
+
+/**
+* @api {put} /v2/robot/repository/interference/:no/:md_id setInterference
+* @apiVersion 0.1.0
+* @apiName setInterference
+* @apiGroup Repository
 *
-* @apiSuccess {string} msg returned message
+* @apiDescription setInterference [md_id] [value] [no] [index]
 *
-* @apiError (Error 5xx) InternalServerError Some error happens
+* @apiExample Example usage:
+* curl -X PUT http://192.168.1.253:9000/v2/robot/repository/interference/:no/:md_id
 *
-* @apiErrorExample Response (example):
-*		HTTP/1.1 500 Internal Server Error
-*		{
-*			"msg":	"internal error"
-*		}
+* @apiUse DefaultHeaderAndExample
+*
+* @apiParam (params) {string} no  	   URL Param: no to set
+* @apiParam (params) {string} md_id    URL Param: metadata id
+* @apiUse RequestBodyIndexAndValue
+*
+* @apiUse DefaultResponsesAndExample
+*/
+
+/**
+* @api {put} /v2/robot/repository/params/:md_id 	setParam
+* @apiVersion 0.1.0
+* @apiName setParam
+* @apiGroup Repository
+*
+* @apiDescription setParam [md_id] [value] [index]
+*
+* @apiExample Example usage:
+* curl -X PUT http://192.168.1.253:9000/v2/robot/repository/params/:md_id
+*
+* @apiUse DefaultHeaderAndExample
+*
+* @apiParam (params) {string} md_id    URL Param: metadata id
+* @apiUse RequestBodyIndexAndValue
+*
+* @apiUse DefaultResponsesAndExample
+*/
+
+/**
+* @api {put} /v2/robot/repository/toolframes/:tool_no/:md_id/pos/:pos_no setToolFrame
+* @apiVersion 0.1.0
+* @apiName setToolFrame
+* @apiGroup Repository
+*
+* @apiDescription setToolFrame [md_id] [value] [tool_no] [pos_no|index] [index]
+*
+* @apiExample Example usage:
+* curl -X PUT http://192.168.1.253:9000/v2/robot/repository/toolframes/:tool_no/:md_id/pos/:pos_no
+*
+* @apiUse DefaultHeaderAndExample
+*
+* @apiParam (params) {string} tool_no  URL Param: tool no to set
+* @apiParam (params) {string} md_id    URL Param: metadata id
+* @apiParam (params) {string} pos_no   URL Param: pos num
+* @apiUse RequestBodyIndexAndValue
+*
+* @apiUse DefaultResponsesAndExample
+*/
+
+/**
+* @api {put} /v2/robot/repository/userframe/:userno/:md_id 	setUserFrame 
+* @apiVersion 0.1.0
+* @apiName setUserFrame
+* @apiGroup Repository
+*
+* @apiDescription setUserFrame [md_id] [value] [userNo]
+*
+* @apiExample Example usage:
+* curl -X PUT http://192.168.1.253:9000/v2/robot/repository/userframe/:userno/:md_id
+*
+* @apiUse DefaultHeaderAndExample
+*
+* @apiParam (params) {string} userno   URL Param: user no
+* @apiParam (params) {string} md_id    URL Param: metadata id
+* @apiUse RequestBodyIndexAndValue
+*
+* @apiUse DefaultResponsesAndExample
+*/
+
+/**
+* @api {put} /v2/robot/repository/zeropoint/:md_id 	setZeroPoint
+* @apiVersion 0.1.0
+* @apiName setZeroPoint
+* @apiGroup Repository
+*
+* @apiDescription setZeroPoint [md_id] [value] [index]
+*
+* @apiExample Example usage:
+* curl -X PUT http://192.168.1.253:9000/v2/robot/repository/zeropoint/:md_id
+*
+* @apiUse DefaultHeaderAndExample
+*
+* @apiParam (params) {string} md_id    URL Param: metadata id
+* @apiUse RequestBodyIndexAndValue
+*
+* @apiUse DefaultResponsesAndExample
 */
