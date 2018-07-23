a9f45ab09307721b9ffb0da0c3f128333bffb183
Tao Yang <yangtao3601@126.com>
2018-07-23 16:21:09 +0800

    add resource

--- thirdparty/mrj/mcresource.c
+++ thirdparty/mrj/mcresource.c
@@ -1,14 +1,167 @@
 #include "mcresource.h"
 
-cJSON* get_dspInfor() {
-	cJSON* item;
+cJSON* getAutorunCycleMode() {
+	cJSON* item = cJSON_CreateNumber(GET_AUTORUN_CYCLEMODE());
+	return item;
+}
+
+cJSON* getTeachMode() {
+	cJSON* item = cJSON_CreateNumber(GET_AUTORUN_CYCLEMODE());
+	return item;
+}
+
+cJSON* getMachinePos() {
+	cJSON* item = cJSON_CreateDoubleArray(RobotRes_MachPos_base(), AXIS_COUNT);
+	return item;
+}
+
+cJSON* getMachinePose() {
+	cJSON* item = cJSON_CreateDoubleArray(RobotRes_MachPose_base(), 6);
+	return item;
+}
+
+cJSON* getAbsPulse() {
+	cJSON* item = cJSON_CreateIntArray(SHARE_RES(abs_pulse), AXIS_COUNT);
+	return item;
+}
+
+cJSON* getAbzPulse() {
+	cJSON* item = cJSON_CreateIntArray(SHARE_RES(abz_pulse), AXIS_COUNT);
+	return item;
+}
+
+cJSON* getCurEncode() {
+	cJSON* item = cJSON_CreateIntArray(SHARE_RES(cur_encode), AXIS_COUNT);
+	return item;
+}
+
+cJSON* getRobotState() {
+	cJSON* item = cJSON_CreateNumber(GetRobotState());
+	return item;
+}
+
+cJSON* getServoReady() {
+	cJSON* item = cJSON_CreateNumber(GetServoReady());
+	return item;
+}
+
+cJSON* getCurLine() {
+	cJSON* item = cJSON_CreateNumber(GetCurLine());
+	return item;
+}
+
+
+cJSON* convertToIntArray(uint8_t* base, int array_size) {
+	int i=0;
+	cJSON* array = cJSON_CreateArray();
+	for (;i<array_size;i++) {
+		cJSON_AddItemToArray(array, cJSON_CreateNumber(base[i]));
+	}
+	return array;
+}
+cJSON* getRobPLC() {
+	cJSON* item = cJSON_CreateObject();
+	cJSON_CreateArray()
+	cJSON_AddItemToObject(item, "PLC_IN", convertToIntArray(SHARE_RES(plc).PLC_IN, IO_IN_NUM));
+	cJSON_AddItemToObject(item, "PLC_OUT", convertToIntArray(SHARE_RES(plc).PLC_OUT, IO_OUT_NUM));
+	cJSON_AddItemToObject(item, "PLC_VIN", convertToIntArray(SHARE_RES(plc).PLC_VIN, IO_VIN_NUM));
+	cJSON_AddItemToObject(item, "PLC_VOUT", convertToIntArray(SHARE_RES(plc).PLC_VOUT, IO_VOUT_NUM));
+	cJSON_AddItemToObject(item, "PLC_M", convertToIntArray(SHARE_RES(plc).PLC_M, IO_M_NUM));
+	return item;
+}
+
+cJSON* getSysvar() {
+	cJSON* item,array;
+	int i=0;
 	item = cJSON_CreateObject();
-	cJSON_AddItemToObject(item, "display_state", cJSON_CreateNumber(GetDisplayMsg_State()));
-	cJSON_AddItemToObject(item, "display_msg", cJSON_CreateString(GetDisplayMsg()));
+
+	cJSON_AddItemToObject(item, "cRobB", cJSON_CreateIntArray(SHARE_RES(sysvar).cRobB, B_COUNT));
+
+	array = cJSON_CreateArray();
+	for (i=0; i<I_COUNT; i++) {
+		cJSON_AddItemToArray(array, cJSON_CreateNumber(SHARE_RES(sysvar).iRobI[i]));
+	}
+	cJSON_AddItemToObject(item, "iRobI", array);
+
+	cJSON_AddItemToObject(item, "dRobD", cJSON_CreateDoubleArray(SHARE_RES(sysvar).dRobD, D_COUNT));
+
+	array = cJSON_CreateArray();
+	for (i=0; i<P_COUNT; i++) {
+		cJSON_AddItemToArray(array, cJSON_CreateDoubleArray(SHARE_RES(sysvar).dRobP[i], AXIS_COUNT))
+	}
+	cJSON_AddItemToObject(item, "dRobP", array);
+
+	array = cJSON_CreateArray();
+	for (i=0; i<V_COUNT; i++) {
+		cJSON_AddItemToArray(array, cJSON_CreateDoubleArray(SHARE_RES(sysvar).dRobP[i], VSub_COUNT))
+	}
+	cJSON_AddItemToObject(item, "dRobV", array);
+
+	return item;
+}
+
+cJSON* getRobotMode() {
+	cJSON* item = cJSON_CreateNumber(GetRobotMode());
+	return item;
+}
+
+cJSON* getLocvar() {
+	cJSON* item,array;
+	int i=0;
+	item = cJSON_CreateObject();
+
+	cJSON_AddItemToObject(item, "cRobLB", cJSON_CreateIntArray(SHARE_RES(sysvar).cRobLB, LB_COUNT));
+
+	cJSON_AddItemToObject(item, "iRobLI", cJSON_CreateIntArray(SHARE_RES(sysvar).iRobLI, LI_COUNT));
+
+	cJSON_AddItemToObject(item, "dRobLD", cJSON_CreateDoubleArray(SHARE_RES(sysvar).dRobLD, LD_COUNT));
+
+	array = cJSON_CreateArray();
+	for (i=0; i<LP_COUNT; i++) {
+		cJSON_AddItemToArray(array, cJSON_CreateDoubleArray(SHARE_RES(sysvar).dRobLP[i], AXIS_COUNT))
+	}
+	cJSON_AddItemToObject(item, "dRobLP", array);
+
+	array = cJSON_CreateArray();
+	for (i=0; i<LV_COUNT; i++) {
+		cJSON_AddItemToArray(array, cJSON_CreateDoubleArray(SHARE_RES(sysvar).dRobLV[i], VSub_COUNT))
+	}
+	cJSON_AddItemToObject(item, "dRobLV", array);
+	return item;
+}
+
+cJSON* getLocvarNum() {
+	cJSON* item = cJSON_CreateNumber(SHARE_RES(locvar_num));
+	return item;
+}
+
+cJSON* getAnalogIoInput() {
+	cJSON* item = cJSON_CreateDoubleArray(SHARE_RES(analog_ioInput), ANALOG_IN_NUM);
+	return item;
+}
+
+cJSON* getServoDirveMode() {
+	cJSON* item = cJSON_CreateNumber(SHARE_RES(servo_dirve_mode));
 	return item;
 }
 
 Item ResourceTable[] = {
-	{"dspInfor", &get_dspInfor},
+	{"autorun_cycleMode", &getAutorunCycleMode},
+	{"teach_mode", &getTeachMode},
+	{"machinePos", &getMachinePos},
+	{"machinePose", &getMachinePose},
+	{"abs_pulse", &getAbsPulse},
+	{"abz_pulse", &getAbzPulse},
+	{"cur_encode", &getCurEncode},
+	{"robotState", &getRobotState},
+	{"servoReady", &getServoReady},
+	{"currentLine", &getCurLine},
+	{"plc", &getRobPLC},
+	{"sysvar", &getSysvar},
+	{"robotMode", &getRobotMode},
+	{"locvar", &getLocvar},
+	{"locvar_num", &getLocvarNum},
+	{"analog_ioInput", &getAnalogIoInput},
+	{"servo_dirve_mode", &getServoDirveMode},
 	{"ENDLINE", NULL},
 };
