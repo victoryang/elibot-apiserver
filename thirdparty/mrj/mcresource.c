baf1bdeca34b47175483a6d4c2e70599fed58d23
Tao Yang <yangtao3601@126.com>
2018-07-23 16:43:08 +0800

    add resource

--- thirdparty/mrj/mcresource.c
+++ thirdparty/mrj/mcresource.c
@@ -55,26 +55,26 @@ cJSON* convertToIntArray(uint8_t* base, int array_size) {
 	int i=0;
 	cJSON* array = cJSON_CreateArray();
 	for (;i<array_size;i++) {
-		cJSON_AddItemToArray(array, cJSON_CreateNumber(base[i]));
+		cJSON_AddItemToArray(array, cJSON_CreateNumber((const int *)base[i]));
 	}
 	return array;
 }
 cJSON* getRobPLC() {
 	cJSON* item = cJSON_CreateObject();
-	cJSON_AddItemToObject(item, "PLC_IN", convertToIntArray((const int *)SHARE_RES(plc).PLC_IN, IO_IN_NUM));
-	cJSON_AddItemToObject(item, "PLC_OUT", convertToIntArray((const int *)SHARE_RES(plc).PLC_OUT, IO_OUT_NUM));
-	cJSON_AddItemToObject(item, "PLC_VIN", convertToIntArray((const int *)SHARE_RES(plc).PLC_VIN, IO_VIN_NUM));
-	cJSON_AddItemToObject(item, "PLC_VOUT", convertToIntArray((const int *)SHARE_RES(plc).PLC_VOUT, IO_VOUT_NUM));
-	cJSON_AddItemToObject(item, "PLC_M", convertToIntArray((const int *)SHARE_RES(plc).PLC_M, IO_M_NUM));
+	cJSON_AddItemToObject(item, "PLC_IN", convertToIntArray(SHARE_RES(plc).PLC_IN, IO_IN_NUM));
+	cJSON_AddItemToObject(item, "PLC_OUT", convertToIntArray(SHARE_RES(plc).PLC_OUT, IO_OUT_NUM));
+	cJSON_AddItemToObject(item, "PLC_VIN", convertToIntArray(SHARE_RES(plc).PLC_VIN, IO_VIN_NUM));
+	cJSON_AddItemToObject(item, "PLC_VOUT", convertToIntArray(SHARE_RES(plc).PLC_VOUT, IO_VOUT_NUM));
+	cJSON_AddItemToObject(item, "PLC_M", convertToIntArray(SHARE_RES(plc).PLC_M, IO_M_NUM));
 	return item;
 }
 
 cJSON* getSysvar() {
-	cJSON* item,array;
+	cJSON *item, *array;
 	int i=0;
 	item = cJSON_CreateObject();
 
-	cJSON_AddItemToObject(item, "cRobB", cJSON_CreateIntArray(SHARE_RES(sysvar).cRobB, B_COUNT));
+	cJSON_AddItemToObject(item, "cRobB", cJSON_CreateIntArray((const int *)SHARE_RES(sysvar).cRobB, B_COUNT));
 
 	array = cJSON_CreateArray();
 	for (i=0; i<I_COUNT; i++) {
@@ -86,13 +86,13 @@ cJSON* getSysvar() {
 
 	array = cJSON_CreateArray();
 	for (i=0; i<P_COUNT; i++) {
-		cJSON_AddItemToArray(array, cJSON_CreateDoubleArray(SHARE_RES(sysvar).dRobP[i], AXIS_COUNT))
+		cJSON_AddItemToArray(array, cJSON_CreateDoubleArray(SHARE_RES(sysvar).dRobP[i], AXIS_COUNT));
 	}
 	cJSON_AddItemToObject(item, "dRobP", array);
 
 	array = cJSON_CreateArray();
 	for (i=0; i<V_COUNT; i++) {
-		cJSON_AddItemToArray(array, cJSON_CreateDoubleArray(SHARE_RES(sysvar).dRobP[i], VSub_COUNT))
+		cJSON_AddItemToArray(array, cJSON_CreateDoubleArray(SHARE_RES(sysvar).dRobP[i], VSub_COUNT));
 	}
 	cJSON_AddItemToObject(item, "dRobV", array);
 
@@ -104,28 +104,40 @@ cJSON* getRobotMode() {
 	return item;
 }
 
-cJSON* getLocvar() {
-	cJSON* item,array;
+cJSON* getEachLocavar(Robot_LOCVAR* base) {
+	cJSON *item, *array;
 	int i=0;
 	item = cJSON_CreateObject();
 
-	cJSON_AddItemToObject(item, "cRobLB", cJSON_CreateIntArray(SHARE_RES(sysvar).cRobLB, LB_COUNT));
+	cJSON_AddItemToObject(item, "cRobLB", cJSON_CreateIntArray(base.cRobLB, LB_COUNT));
 
-	cJSON_AddItemToObject(item, "iRobLI", cJSON_CreateIntArray(SHARE_RES(sysvar).iRobLI, LI_COUNT));
+	cJSON_AddItemToObject(item, "iRobLI", cJSON_CreateIntArray(base.iRobLI, LI_COUNT));
 
-	cJSON_AddItemToObject(item, "dRobLD", cJSON_CreateDoubleArray(SHARE_RES(sysvar).dRobLD, LD_COUNT));
+	cJSON_AddItemToObject(item, "dRobLD", cJSON_CreateDoubleArray(base.dRobLD, LD_COUNT));
 
 	array = cJSON_CreateArray();
 	for (i=0; i<LP_COUNT; i++) {
-		cJSON_AddItemToArray(array, cJSON_CreateDoubleArray(SHARE_RES(sysvar).dRobLP[i], AXIS_COUNT))
+		cJSON_AddItemToArray(array, cJSON_CreateDoubleArray(base.dRobLP[i], AXIS_COUNT));
 	}
 	cJSON_AddItemToObject(item, "dRobLP", array);
 
 	array = cJSON_CreateArray();
 	for (i=0; i<LV_COUNT; i++) {
-		cJSON_AddItemToArray(array, cJSON_CreateDoubleArray(SHARE_RES(sysvar).dRobLV[i], VSub_COUNT))
+		cJSON_AddItemToArray(array, cJSON_CreateDoubleArray(base.dRobLV[i], VSub_COUNT));
 	}
 	cJSON_AddItemToObject(item, "dRobLV", array);
+	return item;
+}
+
+cJSON* getLocvar() {
+	cJSON *item, *array;
+	int i=0;
+	item = cJSON_CreateObject();
+
+	for (i=0; i<CALL_NEST_NUM; i++) {
+		cJSON_AddItemToArray(array, getEachLocavar(SHARE_RES(locvar)[i]));
+	}
+	cJSON_AddItemToObject(item, "locvar", array);
 	return item;
 }
 
