8d040f46c8b4319646ba6197fff14be9d15416a0
Tao Yang <yangtao3601@126.com>
2018-07-31 15:58:23 +0800

    fix

--- travis-ci/install.sh
+++ travis-ci/install.sh
@@ -11,11 +11,11 @@ export MCSERVER=/root/mcserver
 # install gcc-arm tool chain
 sudo apt-get update >/dev/null
 sudo apt-get install -y git curl
-sudo apt-get install -y libc6-armel-cross libc6-dev-armel-cross
-sudo apt-get install -y binutils-arm-linux-gnueabi binutils-arm-linux-gnueabihf
-sudo apt-get install -y libncurses5-dev
-sudo apt-get install -y gcc-arm-linux-gnueabihf
-sudo apt-get install -y g++-arm-linux-gnueabihf
+sudo apt-get install -y libc6-armel-cross libc6-dev-armel-cross >/dev/null
+sudo apt-get install -y binutils-arm-linux-gnueabi binutils-arm-linux-gnueabihf >/dev/null
+sudo apt-get install -y libncurses5-dev >/dev/null
+sudo apt-get install -y gcc-arm-linux-gnueabihf >/dev/null
+sudo apt-get install -y g++-arm-linux-gnueabihf >/dev/null
 
 # include go tool
 sudo mkdir ${GOROOT}
