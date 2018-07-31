8a3d57122e25d4959e48fc6bbafeb0906c396103
Tao Yang <yangtao3601@126.com>
2018-07-31 16:29:17 +0800

    fix

--- travis-ci/install.sh
+++ travis-ci/install.sh
@@ -10,28 +10,9 @@ export MCSERVER=/root/mcserver
 
 # install gcc-arm tool chain
 sudo apt-get update >/dev/null
-sudo apt-get install -y git curl
+sudo apt-get install -y cd
 sudo apt-get install -y libc6-armel-cross libc6-dev-armel-cross >/dev/null
 sudo apt-get install -y binutils-arm-linux-gnueabi binutils-arm-linux-gnueabihf >/dev/null
 sudo apt-get install -y libncurses5-dev >/dev/null
 sudo apt-get install -y gcc-arm-linux-gnueabihf >/dev/null
 sudo apt-get install -y g++-arm-linux-gnueabihf >/dev/null
-
-# include go tool
-# sudo mkdir ${GOROOT}
-# sudo mkdir ${GOPATH}
-# sudo curl -s ${GO_DOWNLOAD_URL}/go${GO_VERSION}.linux-amd64.tar.gz 
-# sudo tar -v -C /usr/local/ -xz go${GO_VERSION}.linux-amd64.tar.gz
-# sudo mkdir -p ${GOPATH}/src ${GOPATH}/bin
-
-# install build dependency
-sudo cd ${GOPATH}/src/
-sudo git clone ${APISERVER_GIT}
-sudo cd elibot-apiserver/build/
-sudo cp lib/libsqlitedb.so /usr/lib/gcc-cross/arm-linux-gnueabihf/4.8
-sudo cp lib/libz.so.1.2.8  /usr/arm-linux-gnueabihf/lib/
-sudo ln -s /usr/arm-linux-gnueabihf/lib/libz.so.1.2.8 /usr/arm-linux-gnueabihf/lib/libz.so.1
-
-sudo mkdir MCSERVER
-sudo tar -xvf include/include.tar.gz -C ${MCSERVER}
-sudo cp lib/libshare.a /usr/arm-linux-gnueabihf/lib/
