8e29212fa2337a2d897247ef9fc8f4cea467df0b
Tao Yang <yangtao3601@126.com>
2018-08-01 11:34:30 +0800

    add docker support

--- README.md
+++ README.md
@@ -7,7 +7,8 @@ A api server for elibot
 ## docker
 docker build -t apiserver:apiserver .
 chmod +x post-install.sh
+pwd
 docker run -it --rm -v '${PWD}:/root/go/src/elibot-apiserver/' apiserver:apiserver
 
 ## build
-chmod +x build/build.sh & sh -C build/build.sh
+chmod +x build/build.sh && sh -C build/build.sh
--- post-install.sh
+++ post-install.sh
@@ -5,7 +5,7 @@ cp build/lib/libz.so.1.2.8 /usr/arm-linux-gnueabihf/lib/
 ln -s /usr/arm-linux-gnueabihf/lib/libz.so.1.2.8 /usr/arm-linux-gnueabihf/lib/libz.so.1
 cp build/lib/libshare.a /usr/arm-linux-gnueabihf/lib/
 mkdir /root/mcserver
-tar -xvf build/include/include.tar.gz -C /root/mcserver/
+tar -xvf build/include/include.tar.gz -C /root/mcserver/ >/dev/null
 cp build/include/config.h /root/mcserver/
 
 /bin/bash
