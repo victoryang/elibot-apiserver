9c694acba8344443aae920bbd039f669f8437637
Tao Yang <yangtao3601@126.com>
2018-08-03 17:40:13 +0800

    add apidocs

--- doc/apidoc/input/resource.go
+++ doc/apidoc/input/resource.go
@@ -1,5 +1,5 @@
 /**
-* @api {get} /v1/robot/resource/sysvar/crobb getcRobB
+* @api {get} /v1/resource/sysvar/crobb?start=:start&end=:end getcRobB
 * @apiVersion 0.1.0
 * @apiName getcRobB
 * @apiGroup Resource
@@ -7,15 +7,18 @@
 * @apiDescription getcRobB
 *
 * @apiExample Example usage:
-* curl -X GET http://192.168.1.253:9000/v1/robot/resource/sysvar/crobb
+* curl -X GET http://192.168.1.253:9000/v1/resource/sysvar/crobb?start=:start&end=:end
 *
 * @apiUse DefaultHeaderAndExample
+*
+* @apiParam (params) {string} start  URL Param: start index
+* @apiParam (params) {string} end    URL Param: end index
 *
 * @apiUse DefaultResponsesAndExample
 */
 
 /**
-* @api {get} /v1/robot/resource/sysvar/irobi getiRobI
+* @api {get} /v1/resource/sysvar/irobi?start=:start&end=:end getiRobI
 * @apiVersion 0.1.0
 * @apiName getiRobI
 * @apiGroup Resource
@@ -23,15 +26,18 @@
 * @apiDescription getiRobI
 *
 * @apiExample Example usage:
-* curl -X GET http://192.168.1.253:9000/v1/robot/resource/sysvar/irobi
+* curl -X GET http://192.168.1.253:9000/v1/resource/sysvar/irobi?start=:start&end=:end
 *
 * @apiUse DefaultHeaderAndExample
+*
+* @apiParam (params) {string} start  URL Param: start index
+* @apiParam (params) {string} end    URL Param: end index
 *
 * @apiUse DefaultResponsesAndExample
 */
 
 /**
-* @api {get} /v1/robot/resource/sysvar/drobd getdRobD
+* @api {get} /v1/resource/sysvar/drobd?start=:start&end=:end getdRobD
 * @apiVersion 0.1.0
 * @apiName getdRobD
 * @apiGroup Resource
@@ -39,15 +45,18 @@
 * @apiDescription getdRobD
 *
 * @apiExample Example usage:
-* curl -X GET http://192.168.1.253:9000/v1/robot/resource/sysvar/drobd
+* curl -X GET http://192.168.1.253:9000/v1/resource/sysvar/drobd?start=:start&end=:end
 *
 * @apiUse DefaultHeaderAndExample
+*
+* @apiParam (params) {string} start  URL Param: start index
+* @apiParam (params) {string} end    URL Param: end index
 *
 * @apiUse DefaultResponsesAndExample
 */
 
 /**
-* @api {get} /v1/robot/resource/sysvar/drobp getdRobP
+* @api {get} /v1/resource/sysvar/drobp?start=:start&end=:end getdRobP
 * @apiVersion 0.1.0
 * @apiName getdRobP
 * @apiGroup Resource
@@ -55,15 +64,18 @@
 * @apiDescription getdRobP
 *
 * @apiExample Example usage:
-* curl -X GET http://192.168.1.253:9000/v1/robot/resource/sysvar/drobp
+* curl -X GET http://192.168.1.253:9000/v1/resource/sysvar/drobp?start=:start&end=:end
 *
 * @apiUse DefaultHeaderAndExample
+*
+* @apiParam (params) {string} start  URL Param: start index
+* @apiParam (params) {string} end    URL Param: end index
 *
 * @apiUse DefaultResponsesAndExample
 */
 
 /**
-* @api {get} /v1/robot/resource/sysvar/drobv getdRobV
+* @api {get} /v1/resource/sysvar/drobv?start=:start&end=:end getdRobV
 * @apiVersion 0.1.0
 * @apiName getdRobV
 * @apiGroup Resource
@@ -71,15 +83,18 @@
 * @apiDescription getdRobV
 *
 * @apiExample Example usage:
-* curl -X GET http://192.168.1.253:9000/v1/robot/resource/sysvar/drobv
+* curl -X GET http://192.168.1.253:9000/v1/resource/sysvar/drobv?start=:start&end=:end
 *
 * @apiUse DefaultHeaderAndExample
+*
+* @apiParam (params) {string} start  URL Param: start index
+* @apiParam (params) {string} end    URL Param: end index
 *
 * @apiUse DefaultResponsesAndExample
 */
 
 /**
-* @api {get} /v1/robot/resource/locvar/croblb/:num getcRobLB
+* @api {get} /v1/resource/locvar/croblb/:num?start=:start&end=:end getcRobLB
 * @apiVersion 0.1.0
 * @apiName getcRobLB
 * @apiGroup Resource
@@ -87,17 +102,19 @@
 * @apiDescription getcRobLB
 *
 * @apiExample Example usage:
-* curl -X GET http://192.168.1.253:9000/v1/robot/locvar/croblb/:num
+* curl -X GET http://192.168.1.253:9000/v1/locvar/croblb/:num?start=:start&end=:end
 *
 * @apiUse DefaultHeaderAndExample
-* 
-* @apiParam (params) {string} num  	   URL Param: num to set
+*
+* @apiParam (params) {string} start  URL Param: start index
+* @apiParam (params) {string} end    URL Param: end index
+* @apiParam (params) {string} num  	 URL Param: num to set
 *
 * @apiUse DefaultResponsesAndExample
 */
 
 /**
-* @api {get} /v1/robot/resource/locvar/irobli/:num getiRobLI
+* @api {get} /v1/resource/locvar/irobli/:num?start=:start&end=:end getiRobLI
 * @apiVersion 0.1.0
 * @apiName getiRobLI
 * @apiGroup Resource
@@ -105,17 +122,19 @@
 * @apiDescription getiRobLI
 *
 * @apiExample Example usage:
-* curl -X GET http://192.168.1.253:9000/v1/robot/locvar/irobli/:num
+* curl -X GET http://192.168.1.253:9000/v1/locvar/irobli/:num?start=:start&end=:end
 *
 * @apiUse DefaultHeaderAndExample
-* 
-* @apiParam (params) {string} num  	   URL Param: num to set
+*
+* @apiParam (params) {string} start  URL Param: start index
+* @apiParam (params) {string} end    URL Param: end index 
+* @apiParam (params) {string} num  	 URL Param: num to set
 *
 * @apiUse DefaultResponsesAndExample
 */
 
 /**
-* @api {get} /v1/robot/resource/locvar/drobld/:num getdRobLD
+* @api {get} /v1/resource/locvar/drobld/:num?start=:start&end=:end getdRobLD
 * @apiVersion 0.1.0
 * @apiName getdRobLD
 * @apiGroup Resource
@@ -123,17 +142,19 @@
 * @apiDescription getdRobLD
 *
 * @apiExample Example usage:
-* curl -X GET http://192.168.1.253:9000/v1/robot/locvar/drobld/:num
+* curl -X GET http://192.168.1.253:9000/v1/locvar/drobld/:num?start=:start&end=:end
 *
 * @apiUse DefaultHeaderAndExample
-* 
-* @apiParam (params) {string} num  	   URL Param: num to set
+*
+* @apiParam (params) {string} start  URL Param: start index
+* @apiParam (params) {string} end    URL Param: end index 
+* @apiParam (params) {string} num  	 URL Param: num to set
 *
 * @apiUse DefaultResponsesAndExample
 */
 
 /**
-* @api {get} /v1/robot/resource/locvar/droblp/:num getdRobLP
+* @api {get} /v1/resource/locvar/droblp/:num?start=:start&end=:end getdRobLP
 * @apiVersion 0.1.0
 * @apiName getdRobLP
 * @apiGroup Resource
@@ -141,17 +162,19 @@
 * @apiDescription getdRobLP
 *
 * @apiExample Example usage:
-* curl -X GET http://192.168.1.253:9000/v1/robot/locvar/droblp/:num
+* curl -X GET http://192.168.1.253:9000/v1/locvar/droblp/:num?start=:start&end=:end
 *
 * @apiUse DefaultHeaderAndExample
-* 
-* @apiParam (params) {string} num  	   URL Param: num to set
+*
+* @apiParam (params) {string} start  URL Param: start index
+* @apiParam (params) {string} end    URL Param: end index 
+* @apiParam (params) {string} num  	 URL Param: num to set
 *
 * @apiUse DefaultResponsesAndExample
 */
 
 /**
-* @api {get} /v1/robot/resource/locvar/droblv/:num getdRobLV
+* @api {get} /v1/resource/locvar/droblv/:num?start=:start&end=:end getdRobLV
 * @apiVersion 0.1.0
 * @apiName getdRobLV
 * @apiGroup Resource
@@ -159,11 +182,13 @@
 * @apiDescription getdRobLV
 *
 * @apiExample Example usage:
-* curl -X GET http://192.168.1.253:9000/v1/robot/locvar/droblv/:num
+* curl -X GET http://192.168.1.253:9000/v1/locvar/droblv/:num?start=:start&end=:end
 *
 * @apiUse DefaultHeaderAndExample
-* 
-* @apiParam (params) {string} num  	   URL Param: num to set
+*
+* @apiParam (params) {string} start  URL Param: start index
+* @apiParam (params) {string} end    URL Param: end index 
+* @apiParam (params) {string} num  	 URL Param: num to set
 *
 * @apiUse DefaultResponsesAndExample
 */
