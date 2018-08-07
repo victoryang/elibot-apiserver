ada672431014a229c0a1dae546962815b9805266
Tao Yang <yangtao3601@126.com>
2018-08-07 14:30:44 +0800

    add date

--- api/apiv1/api-settings.go
+++ api/apiv1/api-settings.go
@@ -12,27 +12,33 @@ func rebootSystem(w http.ResponseWriter, r *http.Request) {
 	cmd := exec.Command("reboot")
 	if err := cmd.Run(); err!=nil {
 		Log.Error("reboot not executed: ", err)
+		return
 	}
 	WriteSuccessResponse(w, "succeed in reboot")
 }
 
 func getSystemDate(w http.ResponseWriter, r *http.Request) {
 	cmd := exec.Command("date")
-	out, err := cmd.Output()
+	stdout, err := cmd.StdoutPipe()
+	cmd.Start()
+	out, err := ioutil.ReadAll(stdout)
 	if err != nil {
-		Log.Error(err)
+		Log.Error("output error: ", err)
+		return
 	}
 	WriteJsonSuccessResponse(w, out)
 }
 
 func setSystemDate(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	date := vars["date"]
-	command := "date -s " + date
-	cmd := exec.Command(command)
-	out, err := cmd.Output()
+	args := " -s " + vars["date"]
+	cmd := exec.Command("date", args)
+	stdout, err := cmd.StdoutPipe()
+	cmd.Start()
+	out, err := ioutil.ReadAll(stdout)
 	if err != nil {
-		Log.Error(err)
+		Log.Error("output error: ", err)
+		return
 	}
 	WriteJsonSuccessResponse(w, out)
 }
