8ef935140fa2bb47c40453bb88b31b95320c2350
Tao Yang <yangtao3601@126.com>
2018-08-23 11:14:53 +0800

    fix

--- thirdparty/mcsql/mcsql.c
+++ thirdparty/mcsql/mcsql.c
@@ -28,7 +28,7 @@ char* mcsql_query_with_param(char* id, char* conn, int32_t type_handle_mode, db_
 	db_query_req req = {
 		query_id: id,
 		conn_str: conn,
-		option: &option,
+		option: &opt,
 		parameter: parameter,
 		page: page,
 	};
