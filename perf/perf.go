9e98d6f824c696fe79db4681326d0cc83d62661c
Tao Yang <yangtao3601@126.com>
2018-09-04 14:54:49 +0800

    add monitor

--- perf/perf.go
+++ perf/perf.go
@@ -12,12 +12,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func StartPerfMonitor() {
 	agent := stackimpact.Start(stackimpact.Options{
-        AgentKey: "elibot-apiserver",
-        AppName: "Basic Go Server",
+        AgentKey: "e32976f8baa8ee4d669221dc2e39d2264f18156c",
+        AppName: "MyGoApp",
         AppVersion: "1.0.0",
         AppEnvironment: "production",
+        DashboardAddress:":5050",
     })
 
-    http.HandleFunc(agent.ProfileHandlerFunc("/", handler))
+    http.HandleFunc(agent.ProfileHandlerFunc("/stackimpact/", handler))
     http.ListenAndServe(":9091", nil)
 }
