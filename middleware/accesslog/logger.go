d79749b96d6e080d75aa64678856f74bfc181e19
Tao Yang <yangtao3601@126.com>
2018-09-11 10:15:46 +0800

    backup access log if greater than 2M

--- middleware/accesslog/logger.go
+++ middleware/accesslog/logger.go
@@ -52,10 +52,8 @@ type Logger struct {
 }
 
 func openAccessLogFile(filePath string) (*os.File, error) {
-	dir := filepath.Dir(filePath)
-
-	if err := os.MkdirAll(dir, 0755); err != nil {
-		return nil, fmt.Errorf("failed to create log path %s: %s", dir, err)
+	if ok := checkFileSizeExceededMax(filePath); ok {
+		os.Rename(filePath, filePath + ".bak")
 	}
 
 	file, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0664)
@@ -67,11 +65,6 @@ func openAccessLogFile(filePath string) (*os.File, error) {
 }
 
 func (l *Logger) Close() error {
-	if ok := checkFileSizeExceededMax(l.file); ok {
-		defer func() {
-			os.Rename(l.filename, l.filename + ".bak")
-		}()
-	}
 	return l.file.Close()
 }
 
