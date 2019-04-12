8f253ea475743131a0952a3b08bab7554e08dc2e
Tao Yang <yangtao3601@126.com>
2019-04-12 15:54:33 +0800

    add resource

--- thirdparty/mrj/mcnv.h
+++
@@ -1,8 +0,0 @@
-#ifndef MCNV_H
-#define MCNV_H
-
-#include "define.h"
-
-cJSON* get_nv();
-
-#endif //MCNV_H
--- thirdparty/mrj/mcresource.h
+++
@@ -1,8 +0,0 @@
-#ifndef MCRESOURCE_H
-#define MCRESOURCE_H
-
-#include "define.h"
-
-cJSON* get_resource();
-
-#endif //MCRESOURCE_H
